ae18a102f716f1d9a28dfc8c9140f0b01febfc5b
BeCaBuCode <23520950@gm.uit.edu.vn>
2025-10-19 16:30:21 +0700

    Models modification: add long and lat to address + change default working status to available

--- models/addresses.js
+++ models/addresses.js
@@ -3,6 +3,8 @@ import mongoose from "mongoose";
 const addressSchema = new mongoose.Schema({
   user_id: { type: mongoose.Schema.Types.ObjectId, ref: "User", required: true },
   detail: { type: String, require: true, trim: true },
+  longtitude: { type: Number, required: true },
+  latitude: { type: Number, required: true },
 });
 
 const Address = mongoose.models.Address || mongoose.model("Address", addressSchema);
