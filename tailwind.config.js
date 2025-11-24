8843e95ca21490e67523632e5922ab69023ce2d1
UIT-20520387 <91862180+UIT-20520387@users.noreply.github.com>
2025-11-24 09:27:43 +0700

    add voucher UI

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,35 +1,36 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: [
-    "./templates/**/*.html",
-    "./*.html"
-  ],
+  content: ["./templates/**/*.html", "./*.html"],
   theme: {
     extend: {
       fontFamily: {
-        montserrat: ['Montserrat', 'sans-serif'], // Tuỳ chỉnh font 'montserrat'
+        montserrat: ["Montserrat", "sans-serif"], // Tuỳ chỉnh font 'montserrat'
       },
       colors: {
         primary: {
-          100: '#EAFED9', // cream
-          200: '#95DA97', // light green
-          400: '#5C956C', // medium green
-          500: '#3B5F43', // dark green
+          100: "#EAFED9", // cream
+          200: "#95DA97", // light green
+          400: "#5C956C", // medium green
+          500: "#3B5F43", // dark green
+        },
+        secondary: {
+          100: "#12A327",
+          200: "#54A312",
         },
         accent: {
-          500: '#FFBE18', //warning yellow
+          500: "#FFBE18", //warning yellow
         },
         gray: {
-          100: '#60655C',
-          400: '#969496',
+          100: "#60655C",
+          400: "#969496",
         },
         dark: {
-          900: '#282A37', 
+          900: "#282A37",
         },
-        white: '#FFFFFF',
-        black: '#000000',
+        white: "#FFFFFF",
+        black: "#000000",
       },
     },
   },
-  plugins: [],
-}
+  plugins: [require('@tailwindcss/forms')],
+};
