ee2ac1851a8046d7f4aba0e730975a782c507ff9
GiaAn1603 <anphamproductions@gmail.com>
2026-01-09 21:05:32 +0700

    Update Admin Routes, Controllers, fetch data for Admin manage Taskers

--- backend/routes/admin.route.js
+++ backend/routes/admin.route.js
@@ -1,12 +1,17 @@
 import express from "express";
 import { isAdmin } from "../middleware/verifyRole.js";
 import { verifyToken } from "../middleware/verifyToken.js";
-import { getAllCustomers, updateTaskerProfile } from "../controllers/admin.controller.js";
+import { approveTasker, getAllCustomers, getAllTaskers, rejectTasker, updateTaskerProfile } from "../controllers/admin.controller.js";
 
 const router = express.Router();
 
-// manage employess and customers
-router.patch("/taskers/update/:taskerId", verifyToken, isAdmin, updateTaskerProfile);
+// Manage Customers
 router.get("/customers/all", verifyToken, isAdmin, getAllCustomers);
+
+// Manage Taskers
+router.get("/taskers/all", verifyToken, isAdmin, getAllTaskers);
+router.patch("/taskers/update/:taskerId", verifyToken, isAdmin, updateTaskerProfile);
+router.patch("/taskers/approve/:taskerId", verifyToken, isAdmin, approveTasker);
+router.patch("/taskers/reject/:taskerId", verifyToken, isAdmin, rejectTasker);
 
 export default router;
